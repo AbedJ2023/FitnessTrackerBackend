a9f3f456f9f1766d3042af9ac568cbfcad83b1dc
Abed <abedj2023@Gmail.com>
2023-03-25 14:04:41 -0500

    passsed getuser, getuserbyusername functions

--- db/users.js
+++ db/users.js
@@ -1,27 +1,64 @@
 const client = require("./client");
+const bcrypt = require("bcrypt");
+const SALT_COUNT = 10;
 
 // database functions
 
 // user functions
 async function createUser({ username, password }) {
-  try{
-      const { rows: [ user ] } = await client.query(`
+  const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
       INSERT INTO users(username, password)
       VALUES($1, $2)
       RETURNING *
-    `, [username, password]);
+    `,
+      [username, hashedPassword]
+    );
+    delete user.password;
 
-      return user;
-  }catch(error){
+    return user;
+  } catch (error) {
     console.error(error);
   }
 }
 
-async function getUser({ username, password }) {}
+async function getUser({ username, password }) {
+  try {
+    const user = await getUserByUsername(username);
+    if (!user) return;
+    const hashedPassword = user.password;
+    const passwordsMatch = await bcrypt.compare(password, hashedPassword);
+    if (!passwordsMatch) return;
+    delete user.password;
+    return user;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 async function getUserById(userId) {}
 
-async function getUserByUsername(userName) {}
+async function getUserByUsername(userName) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+      SELECT *
+      FROM users
+      WHERE username = $1
+     `,
+      [userName]
+    );
+    return user;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 module.exports = {
   createUser,
