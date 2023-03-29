4ceb0855598c69dee0bd6d828f9a19012e35699f
Abed <abedj2023@Gmail.com>
2023-03-29 08:32:19 -0500

    finished all routine_activities functions and tests

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -6,28 +6,124 @@ async function addActivityToRoutine({
   count,
   duration,
 }) {
-  try{
-    const {rows:[routine_activity]} = await client.query(`
+  try {
+    const {
+      rows: [routine_activity],
+    } = await client.query(
+      `
       INSERT INTO routine_activities ("routineId", "activityId", count, duration)
       VALUES ($1, $2, $3, $4)
       RETURNING *;
-    `, [routineId, activityId, count, duration]);
+    `,
+      [routineId, activityId, count, duration]
+    );
 
     return routine_activity;
-  }catch(error){
+  } catch (error) {
     console.error(error);
   }
 }
 
-async function getRoutineActivityById(id) {}
+async function getRoutineActivityById(id) {
+  try {
+    const {
+      rows: [routine_activity],
+    } = await client.query(
+      `
+      SELECT * 
+      FROM routine_activities
+      WHERE id=$1;
+      `,
+      [id]
+    );
+    return routine_activity;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-async function getRoutineActivitiesByRoutine({ id }) {}
+async function getRoutineActivitiesByRoutine({ id }) {
+  try {
+    const { rows: routine_activity } = await client.query(
+      `
+      SELECT routine_activities.* 
+      FROM routine_activities
+      JOIN routines ON routine_activities."routineId"=routines.id
+      WHERE "routineId"=$1;
+      `,
+      [id]
+    );
+    return routine_activity;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-async function updateRoutineActivity({ id, ...fields }) {}
+async function updateRoutineActivity({ id, ...fields }) {
+  {
+    const setString = Object.keys(fields)
+      .map((key, index) => `"${key}"=$${index + 1}`)
+      .join(", ");
+    if (setString.length === 0) {
+      return;
+    }
+    try {
+      const {
+        rows: [activity],
+      } = await client.query(
+        `
+    UPDATE routine_activities
+    SET ${setString}
+    WHERE id=${id}
+    RETURNING *;
+    `,
+        Object.values(fields)
+      );
+      return activity;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
 
-async function destroyRoutineActivity(id) {}
+async function destroyRoutineActivity(id) {
+  try {
+    const {
+      rows: [activity],
+    } = await client.query(
+      `
+      DELETE 
+      FROM routine_activities
+      WHERE id=${id}
+      RETURNING *
+    `
+    );
 
-async function canEditRoutineActivity(routineActivityId, userId) {}
+    return activity;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function canEditRoutineActivity(routineActivityId, userId) {
+  try {
+    const {
+      rows: [activity],
+    } = await client.query(
+      `SELECT * 
+    FROM routine_activities
+    JOIN routines ON routine_activities."routineId"=routines.id
+    WHERE routine_activities.id=${routineActivityId};
+    `
+    );
+
+    if (activity.creatorId === userId) {
+      return activity;
+    } else return;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 module.exports = {
   getRoutineActivityById,
