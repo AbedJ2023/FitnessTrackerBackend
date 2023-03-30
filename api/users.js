fcda04e909fc7d4250278c890ae81a090e5be618
Abed <abedj2023@Gmail.com>
2023-03-30 10:34:24 -0500

    finished user register function

--- api/users.js
+++ api/users.js
@@ -1,8 +1,55 @@
 /* eslint-disable no-useless-catch */
 const express = require("express");
+const { createUser, getUserByUsername } = require("../db");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const { PasswordTooShortError, UserTakenError } = require("../errors");
+const { JWT_SECRET } = process.env;
 
 // POST /api/users/register
+router.post("/register", async (req, res, next) => {
+  const { username, id, password } = req.body;
+  try {
+    const _user = await getUserByUsername(username);
+    if (_user) {
+      res.send({
+        name: "ErrorUserExists",
+        message: UserTakenError(username),
+        error: "error",
+      });
+    }
+
+    if (password.length < 8) {
+      res.send({
+        name: "PasswordLengthError",
+        message: PasswordTooShortError(),
+        error: "error",
+      });
+    }
+
+    const user = await createUser({
+      username,
+      password,
+    });
+
+    const token = jwt.sign(
+      {
+        id: id,
+        username,
+      },
+      JWT_SECRET,
+      { expiresIn: "1w" }
+    );
+
+    res.send({
+      message: "Thank you for registering",
+      token,
+      user,
+    });
+  } catch ({ name, message, error }) {
+    next({ name, message, error });
+  }
+});
 
 // POST /api/users/login
 
--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-require("dotenv").config()
-const express = require("express")
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const app = express();
+const apiRouter = require("./api/index");
+const client = require("./db/client");
 
 // Setup your Middleware and API Router here
+client.connect();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/api", apiRouter);
+
+// app.get("/api/health", function (req, res) {
+//   res.send({ message: "it is healthy" });
+// });
 
 module.exports = app;
